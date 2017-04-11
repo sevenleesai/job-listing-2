d5ee1474c802eba42e9d657cf85c4310ccb5dc95
SAI <sai@SAIdeMacBook-Pro.local>
2017-04-11 12:23:15 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,7 +15,7 @@ class JobsController < ApplicationController
             when 'by_lower_bound'
               Job.published.order('wage_lower_bound DESC')
             when 'by_upper_bound'
-              Job.published.order('wage_lower_bound DESC')
+              Job.published.order('wage_upper_bound DESC')
             else
               Job.published.recent
             end
